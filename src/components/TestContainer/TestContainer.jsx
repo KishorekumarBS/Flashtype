ffa3e0eadc5d81d9d8d8aa10368f75ddd3985a28
KishorekumarBS <hksankaran@gmail.com>
2024-07-06 10:39:41 -0700

    Added testcontainer, and tryagain

--- src/components/TestContainer/TestContainer.jsx
+++ src/components/TestContainer/TestContainer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./TestContainer.css";
+import TryAgain from "../Tryagain/Tryagain";
 
 export default function TestContainer({ words, characters, wpm }) {
   return (
     <div className="test-container">
       <div className="try-again-cont">
-        <h1>This is the try again space</h1>
+        <TryAgain />
       </div>
     </div>
   );
