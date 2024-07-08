c407119f07ed085ffe76c82a644c429883aca3cc
KishorekumarBS <hksankaran@gmail.com>
2024-07-08 16:06:54 -0700

    Updated Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-
 import "./Footer.css";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className="footer-container">
       <a
@@ -13,6 +14,14 @@ const Footer = () => {
       >
         Checkout my Github
       </a>
+      <div className="myinfo">
+        <p>&copy; {currentYear} Kishorekumar. All rights reserved.</p>
+        <p>Built with React, JavaScript (ES6), CSS, and integrated APIs.</p>
+      </div>
+      <p>
+        Contact me:{" "}
+        <a href="mailto:hksankaran@gmail.com">hksankaran@gmail.com</a>
+      </p>
     </div>
   );
 };
